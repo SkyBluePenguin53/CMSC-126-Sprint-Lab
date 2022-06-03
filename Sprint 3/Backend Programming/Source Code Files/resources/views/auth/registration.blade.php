43f538e9491ec70b59d2e00e6c7add12ea495b93
SkyBluePenguin53 <82796132+SkyBluePenguin53@users.noreply.github.com>
2022-06-03 17:26:31 +0800

    Add files via upload

---
+++ Sprint 3/Backend Programming/Source Code Files/resources/views/auth/registration.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/InterArt Global CSS.css') }}">
+    <script src="{{ asset('js/InterArt Gallery JS.js') }}" type="text/javascript"></script>
+	<title>InterArt Gallery Register</title>
+</head>
+
+<body id="MainBody">
+
+<ul class="NavBar">
+<!-- Header portion -->
+
+	<div>
+		<ul id="navi1">
+			<li><a href="/home" id="TitleButton">InterArt Gallery</a></li>
+		</ul>
+	</div>
+	<div>
+		<ul id="navi2">
+			<li><a href="/home/blogs">Find an Art!</a></li>
+			<li><a href="/home/about">About Us</a></li>
+			<li><a href="/home/login">Login</a></li>
+		</ul>
+	</div>
+	<div>
+		<ul id="clock">
+			<span id="hours">00</span>
+			<span>:</span>
+			<span id="minutes">00</span>
+			<span>:</span>
+			<span id="seconds">00</span>
+			<span id="labelTime">-M</span>
+		</ul>
+	</div>
+	
+	
+</ul>
+
+
+<div class="lr-content">
+    <div class="centerVandH">
+	    <h1>Register</h1>
+	    <form action="{{route('register-user')}}" method="post">
+            @if(Session::has('success'))
+	        <div id="accntPosResult">{{Session::get('success')}}</div>
+            @endif
+            @if(Session::has('fail'))
+	        <div id="accntNegResult">{{Session::get('fail')}}</div>
+            @endif
+            @csrf
+		    <label id="lrLabel">Penname</label>
+		    <input type="text" class="lrinputs" name="penname" value="{{old('penname')}}" placeholder="Penname">
+            <label id="lrLabel">Password</label>
+		    <input type="password" class="lrinputs" name="password" placeholder="Password">
+            <span id="accntError">@error('penname'){{$message}} @enderror</span>
+            <br>
+            <span id="accntError">@error('password'){{$message}} @enderror</span>
+
+            <div id="submits">
+			    <input type="submit" name="create" value="Create account"/>
+		    </div>
+	    </form>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ Sprint 3/Backend Programming/Source Code Files/resources/views/homepage.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/InterArt Global CSS.css') }}">
+    <script src="{{ asset('js/InterArt Gallery JS.js') }}" type="text/javascript"></script>
+	<title>InterArt Gallery Homepage</title>
+</head>
+
+<body id="MainBody">
+
+<ul class="NavBar">
+<!-- Header portion -->
+
+	<div>
+		<ul id="navi1">
+			<li><a href="/home" id="TitleButton">InterArt Gallery</a></li>
+		</ul>
+	</div>
+	<div>
+		<ul id="navi2">
+			<li><a href="/home/blogs">Find an Art!</a></li>
+			<li><a href="/home/about">About Us</a></li>
+			<li><a href="/home/login">Login</a></li>
+            <li><a href="/home/registration">Register</a></li>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<ul id="clock">
+			<span id="hours">00</span>
+			<span>:</span>
+			<span id="minutes">00</span>
+			<span>:</span>
+			<span id="seconds">00</span>
+			<span id="labelTime">-M</span>
+		</ul>
+	</div>
+	
+	
+</ul>
+
+<div id="B1" class="BodyH">
+<!-- First Body contents for Homepage -->
+	<div class="HPic">
+		<img src="images/Calabash.png" alt="Creative Artwork Sample">
+	</div>
+
+    <div>
+	    <h1>ONLINE ART GALLERY<br>FOR ART NETIZENS</h1>
+    </div>
+</div>
+
+<div id="B2" class="BodyH">
+<!-- Second Body contents for Homepage -->
+    <div>
+	    <h1>A FREE ART GALLERY<br>OF ANY MEDIUM<br>FOR EVERYONE</h1>
+    </div>
+
+	<div class="HPic">
+		<img src="images/Futurism Laptop.png" alt="Creative Artwork Sample">
+	</div>
+</div>
+
+</body>
+</html>
